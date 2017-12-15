fbfb487440ba9709cdb910a5741937d6b50314fa
jamieRowen <jamie@jumpingrivers.com>
2017-12-15 11:52:56 +0000

    initial commit, port from original course

---
+++ R/import_dummy.R
@@ -0,0 +1,2 @@
+#' @import knitr devtools roxygen2 testthat
+NULL
---
+++ R/jrPackage.R
@@ -0,0 +1,10 @@
+#' R courses by Jumping Rivers
+#' 
+#' Functions and datasets used in for R 
+#' courses run by Jumping Rivers. This package is used in
+#' the "Building a package" course".
+#' @name jrPackage-package
+#' @aliases jrPackage
+#' @docType package
+#' @keywords package
+NULL
